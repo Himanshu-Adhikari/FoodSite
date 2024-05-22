6322783050e1aeb042b41b3bc0669f07830c7bb6
HimanshuAdhikari <adhiakrihimanshu393@gmail.com>
2024-05-22 08:53:05 +0530

    change

---
+++ foodsite/src/Visuals/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+function Home() {
+  return (
+    <>
+    <div>
+      <Nav/>
+      <h1>asdsd</h1>
+      <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default Home
